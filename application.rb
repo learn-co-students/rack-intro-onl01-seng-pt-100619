a34a243c0c314c0fbb0d2c7238f089ca58f47669
Wendy Harris <wendyv510@hotmail.com>
2020-01-11 22:21:53 +0000

    Done.

--- application.rb
+++ application.rb
@@ -1,10 +1,20 @@
+require 'rack' 
+
 class Application
 
-  def call(env)
-    resp = Rack::Response.new
-    resp.write "Hello, World"
-    resp.finish
-  end
-
+ # def call(env)
+  #  resp = Rack::Response.new
+   # resp.write "Hello, World"
+    #resp.finish
+#  end
+  
+   def call(env) 
+     return [200,{'Content-Type' => 'text/html'}, introduction]
+   end 
+   
+   def introduction 
+     (Time.now.to_i).zero?["<em>Hello,my name is"]#:["<strong>Hello,my name is</strong>"]
+   end 
 end
 
+run Application.new 
